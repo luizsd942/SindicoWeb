4f017bef1837220242bcd8755806ec5d2b764dd9
luizsd942 <luizsd942@gmail.com>
2016-02-17 22:04:15 -0200

    Ajustes responnsivos e ajustes nos links para serem chamados tanto pelo localhost quanto pelo servidor externo

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,6 +1,4 @@
-$('#confirmacaoExclusaoModal').on(
-		'show.bs.modal',
-		function(event) {
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 
 			var button = $(event.relatedTarget);
 
@@ -9,7 +7,7 @@ $('#confirmacaoExclusaoModal').on(
 
 			var modal = $(this);
 			var form = modal.find('form');
-			var action = form.data('url-action');
+			var action = form.data('url-base');
 
 			if (!action.endsWith('/')) {
 				action += '/';
